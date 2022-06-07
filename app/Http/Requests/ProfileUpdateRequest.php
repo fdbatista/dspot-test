7eafec57af2a9ae8511d9037002f45ba76c02c61
nico <nico@aleph.engineering>
2022-06-07 17:10:14 -0400

    #3 Refactor ProfileUpdateRequest.php A new function for validating city ID will be executed.

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Http\Rules\IsValidCity;
+use App\Repositories\CityRepository;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
 use JetBrains\PhpStorm\ArrayShape;
@@ -10,7 +11,7 @@ use JetBrains\PhpStorm\ArrayShape;
 class ProfileUpdateRequest extends FormRequest
 {
     #[ArrayShape(['id' => "string", 'phone' => "string", 'first_name' => "string", 'last_name' => "string", 'address' => "string", 'img' => "string", 'zip_code' => "string", 'city_id' => "string"])]
-    public function rules(Request $request): array
+    public function rules(Request $request, CityRepository $cityRepository): array
     {
         $cityId = $request->get('city_id');
 
@@ -22,7 +23,7 @@ class ProfileUpdateRequest extends FormRequest
             'address' => 'required',
             'img' => 'required',
             'zip_code' => 'required',
-            'city_id' => ['required','numeric', new IsValidCity($cityId)],
+            'city_id' => ['required', 'numeric', new IsValidCity($cityId, $cityRepository)],
         ];
     }
 }
