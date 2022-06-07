e32c5d331fb5d09951be0fe60efbc3ffa9154249
nico <nico@aleph.engineering>
2022-06-07 15:29:27 -0400

    #3 Create ProfileController.php

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProfileService;
+use Illuminate\Http\JsonResponse;
+
+class ProfileController extends Controller
+{
+    private ProfileService $profileService;
+
+    public function __construct(ProfileService $profileService)
+    {
+        $this->profileService = $profileService;
+    }
+
+    public function all(): JsonResponse
+    {
+        $data = $this->profileService->all();
+
+        return $this->success($data);
+    }
+}
