ea9ba607fd02465ad8fc617691c0773f988b9b31
nico <nico@aleph.engineering>
2022-06-07 16:17:00 -0400

    #3 Refactor GetAllProfilesEndpointTest.php

---
+++ tests/Feature/GetAllProfilesEndpointTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Repositories\ProfileRepository;
+use Illuminate\Database\Eloquent\Collection;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class GetAllProfilesEndpointTest extends TestCase
+{
+    public function test_get_profiles_returns_collection()
+    {
+        $this->mock(ProfileRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('all')->once()->andReturn(new Collection([['id' => 1]]));
+        });
+
+        $response = $this->get('/api/v1/profile');
+
+        $response->assertJsonCount(1);
+    }
+}
