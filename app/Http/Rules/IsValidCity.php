348b94667b2f81edda2cd48e259a1b64f5585402
nico <nico@aleph.engineering>
2022-06-07 17:10:44 -0400

    #3 Inject CityRepository object into IsValidCity.php

--- app/Http/Rules/IsValidCity.php
+++ app/Http/Rules/IsValidCity.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Rules;
 
-use App\Models\Entities\City;
+use App\Repositories\CityRepository;
 use Illuminate\Contracts\Validation\Rule;
 
 class IsValidCity implements Rule
 {
-    public int $cityId;
+    public ?int $cityId;
+    private CityRepository $cityRepository;
 
-    public function __construct(int $cityId)
+    public function __construct(?int $cityId, CityRepository $cityRepository)
     {
         $this->cityId = $cityId;
+        $this->cityRepository = $cityRepository;
     }
 
     public function passes($attribute, $value): bool
     {
-        return (bool)City::find($this->cityId);
+        return $this->cityRepository->exists(['id' => $this->cityId]);
     }
 
     public function message(): string
