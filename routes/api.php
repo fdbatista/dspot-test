1814d936218963ace17f050c1c6908106245510c
nico <nico@aleph.engineering>
2022-06-07 15:28:54 -0400

    #3 Register route for getting profiles list

--- routes/api.php
+++ routes/api.php
@@ -1,33 +1,12 @@
 <?php
 
-use App\Http\Controllers\EarningController;
-use App\Http\Controllers\JwtController;
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\JwtAuth;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')
     ->group(function () {
-        Route::prefix('jwt')
-            ->controller(JwtController::class)
-            ->group(function () {
-                Route::get('/', 'renewJwt');
-            });
-
-        Route::prefix('users')
-            ->middleware([JwtAuth::class])
-            ->controller(UserController::class)->group(function () {
-                Route::prefix('consultants')->group(function () {
-                    Route::get('/', 'getConsultants');
-                });
-            });
-
-        Route::prefix('earnings')
-            ->middleware([JwtAuth::class])
-            ->controller(EarningController::class)->group(function () {
-                Route::get('/', 'getEarnings');
-                Route::get('/fixed-cost-avg', 'getFixedCostAverage');
-                Route::get('/percent', 'getPercentsByConsultant');
-            });
+        Route::prefix('profile')->controller(ProfileController::class)->group(function () {
+            Route::get('/', 'all');
+        });
     });
 
