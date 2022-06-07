bfbfde0ef0d8245d1c6d3dd5ab7604efb346a0e6
nico <nico@aleph.engineering>
2022-06-07 17:17:08 -0400

    #3 Create CreateProfileEndpointTest.php

---
+++ tests/Feature/CreateProfileEndpointTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Constants\ProfileConstants;
+use App\Repositories\CityRepository;
+use App\Repositories\ProfileRepository;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class CreateProfileEndpointTest extends TestCase
+{
+    public function test_create_profile_returns_validation_error()
+    {
+        $response = $this->post('/api/v1/profile', []);
+
+        $response->assertSeeText('required');
+    }
+
+    public function test_create_profile_returns_successful_message()
+    {
+        $this->mock(CityRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('exists')->once()->andReturn(true);
+        });
+
+        $this->mock(ProfileRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('insert')->once();
+        });
+
+        $response = $this->post('/api/v1/profile', [
+            'phone' => '0018946545',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'address' => '223 Baker St',
+            'img' => 'https://image.to',
+            'zip_code' => 3312,
+            'city_id' => 1,
+        ]);
+
+        $response->assertSeeText(ProfileConstants::SUCCESSFUL_OPERATION_MESSAGE);
+    }
+}
