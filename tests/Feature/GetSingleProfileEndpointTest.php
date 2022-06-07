4e5b8292c9659656842bb75dd859694c9277963f
nico <nico@aleph.engineering>
2022-06-07 16:17:10 -0400

    #3 Create GetSingleProfileEndpointTest.php

---
+++ tests/Feature/GetSingleProfileEndpointTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Entities\Profile;
+use App\Repositories\ProfileRepository;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class GetSingleProfileEndpointTest extends TestCase
+{
+    public function test_get_a_single_profile_returns_object()
+    {
+        $this->mock(ProfileRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('find')->once()->andReturn(new Profile(['id' => 1]));
+        });
+
+        $response = $this->get('/api/v1/profile/1');
+
+        $response->assertStatus(200);
+    }
+}
