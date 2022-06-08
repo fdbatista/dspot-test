e76e4089e08b7c3236c817bb98c09194160c9e02
nico <nico@aleph.engineering>
2022-06-08 13:59:00 -0400

    #5 Refactor hops method return value calculation Getting all models at once is more efficient.

--- app/Http/Controllers/FriendsController.php
+++ app/Http/Controllers/FriendsController.php
@@ -25,17 +25,8 @@ class FriendsController extends Controller
     public function findShorterPath(int $origin, int $destination): JsonResponse
     {
         $hops = $this->friendsService->findShorterPath($origin, $destination);
-
-        $result = array_map(function ($hop) {
-            return $this->getProfileDescription($hop);
-        }, $hops);
+        $result = $this->profileService->findModels($hops);
 
         return $this->success($result);
-    }
-
-    private function getProfileDescription(int $id): string {
-        $profileModel = $this->profileService->find($id);
-
-        return $profileModel->first_name . ' ' . $profileModel->last_name . ' - ' . $profileModel->id;
     }
 }
