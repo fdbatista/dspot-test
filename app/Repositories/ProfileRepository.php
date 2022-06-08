6106904b98f3afc2d099a5fa3b9c606e582e3785
nico <nico@aleph.engineering>
2022-06-08 13:59:25 -0400

    #5 Create 'findModels' method on ProfileRepository.php

--- app/Repositories/ProfileRepository.php
+++ app/Repositories/ProfileRepository.php
@@ -41,4 +41,15 @@ class ProfileRepository extends AbstractRepository
             ->join('state', 'city.state_id', '=', 'state.id')
             ->select(ProfileConstants::PROFILE_ATTRIBS_TO_RETURN);
     }
+
+    public function findModels(array $ids): array
+    {
+        return $this->whereIn('id', $ids)
+            ->select(['id', 'first_name', 'last_name'])
+            ->get()
+            ->map(function ($item) {
+                return $item->first_name . ' ' . $item->last_name . ' - ' . $item->id;
+            })
+            ->toArray();
+    }
 }
