e11c9e5245d2658fac900e27806fc1a4a2ddec59
nico <nico@aleph.engineering>
2022-06-08 00:51:04 -0400

    #4 Add findShorterPath method to FriendsController.php

--- app/Http/Controllers/FriendsController.php
+++ app/Http/Controllers/FriendsController.php
@@ -20,4 +20,21 @@ class FriendsController extends Controller
 
         return $this->success($result);
     }
+
+    public function findShorterPath(int $origin, int $destination): JsonResponse
+    {
+        $hops = $this->friendsService->findShorterPath($origin, $destination);
+
+        $result = array_map(function ($hop) {
+            return $this->getProfileDescription($hop);
+        }, $hops);
+
+        return $this->success($result);
+    }
+
+    private function getProfileDescription(int $id): string {
+        $profileModel = $this->profileService->find($id);
+
+        return $profileModel->first_name . ' ' . $profileModel->last_name . ' - ' . $profileModel->id;
+    }
 }
