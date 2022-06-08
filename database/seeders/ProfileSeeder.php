d738ab0c4ab7cc8d2136c22bce3bfc6c1e7ba748
nico <nico@aleph.engineering>
2022-06-08 18:41:20 -0400

    #5 Persist users generated from API response on ProfileSeeder.php

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -2,13 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Repositories\ProfileRepository;
+use Database\Factories\ProfileFactory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class ProfileSeeder extends Seeder
 {
+    public function __construct(
+        private ProfileFactory $profileFactory,
+        private ProfileRepository $profileRepository,
+    )
+    {
+    }
+
     public function run(int $profilesTotal, int $friendsTotal)
     {
-        Log::debug("Seeder params", [$profilesTotal, $friendsTotal]);
+        $users = $this->profileFactory->getFakeProfiles($profilesTotal);
+
+        DB::transaction(function () use ($users) {
+            $this->profileRepository->deleteAll();
+            $this->profileRepository->insert($users);
+        });
     }
 }
