63bf9be6ad871a4b2b1a73addc152131a209c257
nico <nico@aleph.engineering>
2022-06-09 12:37:22 -0400

    #5 Create method 'raiseExceptionIfInvalidNumberOfConnections' on ProfileSeeder.php

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -2,23 +2,40 @@
 
 namespace Database\Seeders;
 
+use App\Repositories\FriendRepository;
 use App\Repositories\ProfileRepository;
+use App\Utils\MaxFriendsCalculatorUtil;
 use Database\Factories\ProfileFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use OutOfRangeException;
 
 class ProfileSeeder extends Seeder
 {
     public function __construct(
         private ProfileFactory $profileFactory,
         private ProfileRepository $profileRepository,
+        private FriendRepository $friendRepository,
     )
     {
     }
 
     public function run(int $profilesTotal, int $friendsTotal)
     {
-        $users = $this->profileFactory->getFakeProfiles($profilesTotal);
+        $this->raiseExceptionIfInvalidNumberOfConnections($profilesTotal, $friendsTotal);
+        $this->runSeed($profilesTotal, $friendsTotal);
+    }
+
+    private function raiseExceptionIfInvalidNumberOfConnections(int $profilesTotal, int $friendsTotal) {
+        $maxPossibleFriendsCombinations = MaxFriendsCalculatorUtil::findMaxCombinationsAvailable($profilesTotal);
+
+        if ($friendsTotal > $maxPossibleFriendsCombinations) {
+            throw new OutOfRangeException("$profilesTotal profiles cannot have more than $maxPossibleFriendsCombinations friends.");
+        }
+    }
+
+    private function runSeed(int $profilesTotal, int $friendsTotal) {
+        $profiles = $this->profileFactory->buildFakeProfiles($profilesTotal);
 
         DB::transaction(function () use ($profiles, $friendsTotal) {
             $this->createProfiles($profiles);
