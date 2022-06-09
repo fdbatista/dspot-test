0929a75a953d81fc307be3f273e6a337eec498ec
nico <nico@aleph.engineering>
2022-06-09 15:23:34 -0400

    #6 Reformat api.php

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,16 @@ use App\Http\Controllers\FriendsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')
-    ->group(function () {
-        Route::prefix('profile')->controller(ProfileController::class)->group(function () {
-            Route::get('/', 'all');
-            Route::get('/{id}', 'find');
-            Route::put('/', 'update');
-            Route::post('/', 'create');
-        });
-
-        Route::prefix('friends')->controller(FriendsController::class)->group(function () {
-            Route::get('/{id}', 'findFriends');
-            Route::get('/path/{origin}/{destination}', 'findShorterPath');
-        });
+Route::prefix('v1')->group(function () {
+    Route::prefix('profile')->controller(ProfileController::class)->group(function () {
+        Route::get('/', 'all');
+        Route::get('/{id}', 'find');
+        Route::put('/', 'update');
+        Route::post('/', 'create');
     });
+
+    Route::prefix('friends')->controller(FriendsController::class)->group(function () {
+        Route::get('/{id}', 'findFriends');
+        Route::get('/path/{origin}/{destination}', 'findShorterPath');
+    });
+});
