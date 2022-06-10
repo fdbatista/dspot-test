e6a1a7e28115021ee593c5d50bb11c2d499be73a
nico <nico@aleph.engineering>
2022-06-09 23:24:17 -0400

    #7 Create ProfileErrors.php This class will hold constant values for validation middlewares.

---
+++ app/Models/Constants/ProfileErrors.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models\Constants;
+
+class ProfileErrors
+{
+    const INVALID_CITY = 'Invalid city.';
+    const EXISTING_MODEL = 'This combination of first name, last name and phone already exists.';
+}
