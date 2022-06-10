8916b1c630044f5f37ca92d073f5a438568166c5
nico <nico@aleph.engineering>
2022-06-10 11:55:39 -0400

    #8 Refactor ProfileController.php Data will be returned from the parent's added function.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileCreateRequest;
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Constants\ProfileConstants;
 use App\Services\ProfileService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class ProfileController extends Controller
 {
@@ -20,23 +22,22 @@ class ProfileController extends Controller
     {
         $data = $this->profileService->all();
 
-        return $this->success($data);
+        return $this->returnData($data);
     }
 
     public function find(int $id): JsonResponse
     {
         $data = $this->profileService->find($id);
 
-        return $this->success($data);
+        return $this->returnData($data);
     }
 
     public function update(ProfileUpdateRequest $request): JsonResponse
     {
         $data = $request->all(['id', 'phone', 'first_name', 'last_name', 'address', 'img', 'zip_code', 'city_id']);
-
         $this->profileService->update($data);
 
-        return $this->success();
+        return $this->success(ProfileConstants::SUCCESSFUL_OPERATION_MESSAGE, Response::HTTP_CREATED);
     }
 
     public function create(ProfileCreateRequest $request): JsonResponse
@@ -45,6 +46,6 @@ class ProfileController extends Controller
 
         $this->profileService->create($data);
 
-        return $this->success();
+        return $this->success(ProfileConstants::SUCCESSFUL_OPERATION_MESSAGE, Response::HTTP_CREATED);
     }
 }
