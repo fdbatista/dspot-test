26e69267bffc4020b6a40bf3c42f96767673cc0b
nico <nico@aleph.engineering>
2022-06-10 11:53:50 -0400

    #8 Create 'returnData' method on Controller.php This method will be used for returning the proper HTTP code in responses.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\ResponseHandler;
+use App\Utils\EmptyResponseEvaluator;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
+use JetBrains\PhpStorm\Pure;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ResponseHandler;
+
+    protected function returnData(mixed $data): JsonResponse
+    {
+        $httpCode = $this->getResponseHttpCode($data);
+
+        return $this->success($data, $httpCode);
+    }
+
+    #[Pure]
+    private function getResponseHttpCode(mixed $data): int
+    {
+        return EmptyResponseEvaluator::isEmptyResponse($data)
+            ? Response::HTTP_NO_CONTENT
+            : Response::HTTP_OK;
+    }
 }
