8f3974045e973dfa2ac850efe2e7fca43e4dc973
nico <nico@aleph.engineering>
2022-09-14 15:54:22 -0400

    #9 Catch potential exceptions on HttpClientService.php

--- app/Services/HttpClientService.php
+++ app/Services/HttpClientService.php
@@ -2,12 +2,32 @@
 
 namespace App\Services;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
+use JetBrains\PhpStorm\Pure;
+use stdClass;
 
 class HttpClientService
 {
     public function sendRequest(string $url): object|array
     {
-        return Http::get($url)->object();
+        try {
+            $result = Http::get($url)->object();
+
+            return (empty($result))
+                ? self::returnEmptyResponse()
+                : $result;
+        } catch (Exception) {
+            return self::returnEmptyResponse();
+        }
+    }
+
+    #[Pure]
+    private static function returnEmptyResponse(): object
+    {
+        $result = new stdClass();
+        $result->results = [];
+
+        return $result;
     }
 }
